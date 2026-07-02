93a3769ee31da0077b86e72fe84b4571b5608ae4
HO,SHAO-WEI <scarlet8714@gmail.com>
2026-07-02 23:47:02 +0800

    fix: add correct class

--- ip_web_stream3.py
+++ ip_web_stream3.py
@@ -39,8 +39,8 @@ def gen_frames():
         if not success:
             break
 
-        detections = model(frame)
-        frame = model.draw(frame, detections, class_names=["marker"])
+        # detections = model(frame)
+        # frame = model.draw(frame, detections, class_names=["car"])
 
         ret, buffer = cv2.imencode(".jpg", frame)
 
