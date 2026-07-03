14265308f576e5387868141cee5a960a52805c34
HO,SHAO-WEI <scarlet8714@gmail.com>
2026-07-03 16:31:29 +0800

    feat: let every frame detected

--- ip_web_stream3.py
+++ ip_web_stream3.py
@@ -11,7 +11,7 @@ print("Camera height:", camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print("Camera FPS:", camera.get(cv2.CAP_PROP_FPS))
 
 model = TRTDetector(
-    engine_path="model/best2.engine",
+    engine_path="model/best_fp16.engine",
     input_size=640,
     conf_thres=0.25,
     iou_thres=0.45
